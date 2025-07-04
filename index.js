d6c0a35801ec91156de8c5e024626b6b92b41f11
monika-sulecka <monika-sulecka@wp.pl>
2025-07-04 22:45:23 +0200

    Add new style to style.css

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req,res) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, 'public/helloworld.html'));
 })
 
 app.listen(port, () => {
